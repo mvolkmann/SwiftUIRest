d02ae1b99b3dfdbefb7cb923c896aeebf625f1cb
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-22 20:15:14 -0500

    Initial Commit

---
+++ SwiftUIRest/SwiftUIRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRestApp.swift
+//  SwiftUIRest
+//
+//  Created by R. Mark Volkmann on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
